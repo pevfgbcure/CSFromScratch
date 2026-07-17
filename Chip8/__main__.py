4ca96a5e4140e142704ecb7f12c584a463794cf8
Cris <33710623+pevfgbcure@users.noreply.github.com>
2026-07-17 00:27:13 -0600

    feat: Add beep sound and handling of events, sound and timing.
    The run function in __main__ now handles keyboard and sound events
    using the pygame library.
    
    The handling of timing at the end is necessary so the VM will run at the
    original 60 Hz of the original computers of the 70s, otherwise most
    games will be unplayable. Still, the speed of the VM (and of the games
    it plays) can be changed by modifying the FRAME_TIME_EXPECTED constant.

--- Chip8/__main__.py
+++ Chip8/__main__.py
@@ -1,8 +1,16 @@
 import os
+import sys
 from timeit import default_timer as timer
 
 import pygame
-from Chip8.vm import SCREEN_HEIGHT, SCREEN_WIDTH, VM  # type: ignore
+from Chip8.vm import (  # type: ignore
+    ALLOWED_KEYS,
+    FRAME_TIME_EXPECTED,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    TIMER_DELAY,
+    VM,
+)
 
 
 def run(program_data: bytes, name: str):
@@ -10,8 +18,8 @@ def run(program_data: bytes, name: str):
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
     pygame.display.set_caption(f"Chip8 - {os.path.basename(name)}")
-    bee_sound = pygame.mixer.Sound(
-        os.path.dirname(os.path.realpath(__file__)) + "/bee.wav"
+    beep_sound = pygame.mixer.Sound(
+        os.path.dirname(os.path.realpath(__file__)) + "/beep.wav"
     )
     currently_playing_sound = False
     vm = VM(program_data)  # load the virtual machine with the program data.
@@ -23,3 +31,39 @@ def run(program_data: bytes, name: str):
         if vm.needs_redraw:
             pygame.surfarray.blit_array(screen, vm.display_buffer)
             pygame.display.flip()
+
+        # Handle keyboard events.
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                key_name = pygame.key.name(event.key)
+                if key_name in ALLOWED_KEYS:
+                    vm.keys[ALLOWED_KEYS.index(key_name)] = True
+            elif event.type == pygame.KEYUP:
+                key_name = pygame.key.name(event.key)
+                if key_name in ALLOWED_KEYS:
+                    vm.keys[ALLOWED_KEYS.index(key_name)] = False
+            elif event.type == pygame.QUIT:
+                sys.exit()
+
+        # Sound.
+        if vm.play_sound:
+            if not currently_playing_sound:
+                beep_sound.play(-1)
+                currently_playing_sound = True
+            else:
+                currently_playing_sound = False
+                beep_sound.stop()
+
+        # Handle timing.
+        frame_end = timer()
+        frame_time = frame_end - frame_start  # time the frame took in seconds.
+        timer_accumulator += frame_time
+        # Every 1/60 of a second decrement the timers.
+        if timer_accumulator > TIMER_DELAY:
+            vm.decrement_timers()
+            timer_accumulator = 0
+        # Limit the speed of the entire machine to FRAME_TIME_EXPECTED "frames" per second.
+        if frame_time < FRAME_TIME_EXPECTED:
+            difference = FRAME_TIME_EXPECTED - frame_time
+            pygame.time.delay(int(difference * 1000))
+            timer_accumulator += difference
